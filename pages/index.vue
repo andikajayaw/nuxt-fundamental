dab39ebd5f99d87d70dc08974e7ab13b979949f2
LAPTOP-E5999C64\SiNDATA - ANDIKA <andikajayawiguna@gmail.com>
2022-09-19 10:49:34 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="home">
+    <!-- Hero -->
+    <Hero />
+
+    <!-- Search -->
+    <div class="container search">
+      <input
+        v-model.lazy="search"
+        type="text"
+        placeholder="Search..."
+        @keyup.enter="$fetch"
+      />
+      <button v-show="search.length > 0" class="button" @click="clearSearch">
+        X
+      </button>
+    </div>
+
+    <!-- Movies -->
+    <MovieCard :listMovie="movies" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  loading: false,
+  data() {
+    return {
+      movies: [],
+      search: '',
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getSearchMovies() {
+      this.$nuxt.$loading.start()
+      const res = await axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=637229d93973db648ccec07e3acd8553&language=en-US&page=1&query=${this.search}`
+        )
+        .then((response) => response.data)
+        .catch((error) => alert(error.message))
+      this.movies = []
+      res.results.forEach((element) => {
+        // console.log(element)
+        this.movies.push(element)
+      })
+      this.$nuxt.$loading.finish()
+    },
+    async getMovies() {
+      if (this.search.length > 0) {
+        await this.getSearchMovies()
+        return
+      }
+      const data = await axios.get(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=637229d93973db648ccec07e3acd8553&language=en-US&page=1'
+      )
+      const { results } = data.data
+      this.movies = []
+      results.forEach((element) => {
+        this.movies.push(element)
+      })
+    },
+    async clearSearch() {
+      this.search = ''
+      await this.$fetch()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.search {
+  display: flex;
+  padding: 32px 16px;
+  input {
+    max-width: 350px;
+    width: 100%;
+    padding: 12px 6px;
+    font-size: 14px;
+    border: none;
+    &:focus {
+      outline: none;
+    }
+  }
+  .button {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+}
+</style>
